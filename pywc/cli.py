f9b2ee72ddaad14fa8a0ef650a1738872546b368
Emil Kloeden <emilkloeden@gmail.com>
2017-06-12 16:24:11 +0930

    removed pypy from travis since crypto@1.9 doesn't work with pypy<5.3

--- pywc/cli.py
+++ pywc/cli.py
@@ -1,14 +1,65 @@
 # -*- coding: utf-8 -*-
 
 import click
+import pywc
+import sys
+import os
 
 
 @click.command()
-def main(args=None):
-    """Console script for pywc"""
-    click.echo("Replace this message by putting your code into "
-               "pywc.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.option('--bytes_', '-c', is_flag=True, help="""
+The number of bytes in each input file is written to the standard
+output.  This will cancel out any prior usage of the -m option.""")
+@click.option('--lines', '-l', is_flag=True, help="""
+The number of lines in each input file is written to the standard
+output.""")
+@click.option('--chars', '-m', is_flag=True, help="""
+The number of characters in each input file is written to the
+standard output.  If the current locale does not support multi-
+byte characters, this is equivalent to the -c option.  This will
+cancel out any prior usage of the -c option.""")
+@click.option('--words', '-w', is_flag=True, help="""
+The number of words in each input file is written to the standard
+output.""")
+
+@click.argument('filenames', nargs=-1)
+def main(bytes_, chars, lines, words, filenames):
+    """
+    When an option is specified, wc only reports the information requested by
+    that option.  The order of output always takes the form of line, word,
+    byte, and file name.  The default action is equivalent to specifying the
+    -c, -l and -w options.
+
+    If no files are specified, the standard input is used and no file name is
+    displayed.  The prompt will accept input until receiving EOF, or [^D] in
+    most environments.
+    """
+    first_file = filenames[0]
+
+    if os.path.isfile(first_file):
+        for filename in filenames:
+            with open(filename, 'r') as fp:
+                tuple_ = pywc.disambiguate(
+                    bytes_,
+                    chars,
+                    lines,
+                    words,
+                    fp,
+                    filename=os.path.basename(filename)
+                    )
+                click.echo(pywc.to_string(*tuple_))
+
+    else:
+        tuple_ = pywc.disambiguate(
+            bytes_,
+            chars,
+            lines,
+            words,
+            filenames,
+            filename=None
+        )
+        click.echo(pywc.to_string(*tuple_))
+
 
 
 if __name__ == "__main__":
--- pywc/pywc.py
+++ pywc/pywc.py
@@ -1,1 +1,70 @@
 # -*- coding: utf-8 -*-
+import sys
+import os
+
+#sys.stdin
+
+def count_lines(file_object):
+    return len(file_object.readlines())
+
+def count_words(file_object):
+    return len(file_object.read().split())
+
+def count_bytes(file_object):
+    return len(file_object.read().encode('utf-8'))
+
+def count_chars(file_object):
+    return len(file_object.read())
+
+
+def to_string(line_count, byte_count, word_count, char_count, filename):
+    c,l,m,w = '','','',''
+    if line_count is not None:
+        l = str(line_count).rjust(8)
+    if byte_count is not None:
+        c = str(byte_count).rjust(8)
+    if word_count is not None:
+        w = str(word_count).rjust(8)
+    if char_count is not None:
+        m = str(char_count).rjust(8)
+
+    return "{}{}{}{} {}".format(
+        c,l,m,w,
+        filename
+    )
+
+def disambiguate(bytes_, chars, lines, words, fp, filename=None):
+    """
+    When an option is specified, wc only reports the information requested by
+    that option.  The order of output always takes the form of line, word,
+    byte, and file name.  The default action is equivalent to specifying the
+    -c, -l and -w options.
+
+    If no files are specified, the standard input is used and no file name is
+    displayed.  The prompt will accept input until receiving EOF, or [^D] in
+    most environments.
+
+    -c bytes
+    -l lines
+    -m chars
+    -w words
+    """
+    line_count, byte_count, word_count, char_count = None, None, None, None
+
+    if (not bytes_) and (not chars) and (not lines) and (not words):
+        lines, words, bytes_ = True, True, True
+
+    if lines:
+        line_count = count_lines(fp)
+
+    if bytes_ and (not chars):
+        byte_count = count_bytes(fp)
+
+    if words:
+        word_count = count_words(fp)
+
+    if chars:
+        char_count = count_chars(fp)
+
+
+    return line_count, byte_count, word_count, char_count, filename
